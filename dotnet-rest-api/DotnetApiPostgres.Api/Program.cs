156f3b6e9efa2b47d53f7f40f040466aeb24b9cf
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-02-25 13:08:08 +0700

    feat: setup db and migration

--- dotnet-rest-api/DotnetApiPostgres.Api/Program.cs
+++ dotnet-rest-api/DotnetApiPostgres.Api/Program.cs
@@ -1,11 +1,18 @@
+using DotnetApiPostgres.Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("default");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
