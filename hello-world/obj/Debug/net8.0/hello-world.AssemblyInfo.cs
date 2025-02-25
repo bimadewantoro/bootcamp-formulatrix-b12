b0729a15b40ff160adc4168939527a09725e68ef
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-02-25 10:16:36 +0700

    feat: setup gitignore

--- hello-world/obj/Debug/net8.0/hello-world.AssemblyInfo.cs
+++ hello-world/obj/Debug/net8.0/hello-world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8f78ca60becd474af96c45d183e9d83d0bdf37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ffe1b6ce2a06ab10dac18b16a99fb8f4937b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
