0905ef9865df33506ddfca3dc15f2cc67668dacd
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-02-25 10:33:52 +0700

    feat: logic excercise task

--- logic-exercise/Program.cs
+++ logic-exercise/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 15;
+for (int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("foobar");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("foo");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("bar");
+    }
+    else
+    {
+        Console.Write(i);
+    }
+
+    if (i < n)
+    {
+        Console.Write(", ");
+    }
+}
