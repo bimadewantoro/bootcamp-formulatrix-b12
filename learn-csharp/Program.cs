a9d82bef3fc666898be8636f9d99d489f1e09901
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-02-28 09:54:03 +0700

    feat: setup learn project dir

--- learn-csharp/Program.cs
+++ learn-csharp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a topic to run.");
+                return;
+            }
+
+            switch (args[0].ToLower())
+            {
+                case "hello":
+                    HelloWorld.Run();
+                    break;
+                case "syntax":
+                    Syntax.Run();
+                    break;
+                default:
+                    Console.WriteLine("Unknown topic.");
+                    break;
+            }
+        }
+    }
+}
