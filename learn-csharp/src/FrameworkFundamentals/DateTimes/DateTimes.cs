d609f43a7fb2f73fd78bb55aff7da5312571fda9
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-04 11:48:28 +0700

    learn: date times

--- learn-csharp/src/FrameworkFundamentals/DateTimes/DateTimes.cs
+++ learn-csharp/src/FrameworkFundamentals/DateTimes/DateTimes.cs
@@ -7,6 +7,8 @@ namespace FrameworkFundamental.DateTimes
             // Timespan
             var timeSpan = new TimeSpan(1, 2, 3);
             var timeSpan1 = TimeSpan.FromDays(1);
+            Console.WriteLine("TimeSpan: " + timeSpan);
+            Console.WriteLine("TimeSpan1: " + timeSpan1);
 
             var start = DateTime.Now;
             var end = DateTime.Now.AddMinutes(2);
@@ -17,6 +19,8 @@ namespace FrameworkFundamental.DateTimes
 
             // Date Time
             var dateTime = new DateTime(2015, 1, 1);
+            Console.WriteLine("DateTime: " + dateTime);
+
             var now = DateTime.Now;
 
             Console.WriteLine("Hour: " + now.Hour);
