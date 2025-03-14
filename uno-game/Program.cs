cbf16e5e785e4eb78e0cba4e83e9ad0062d4c082
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-14 10:48:22 +0700

    fix: draw four and challenge not working on draw card

--- uno-game/Program.cs
+++ uno-game/Program.cs
@@ -249,13 +249,34 @@ namespace UnoGame
 
                     if (display.GetYesNoInput())
                     {
+                        bool isWildDrawFour = drawnCard.Effect == Effect.WildDrawFour;
+
                         game.PlayCard(player, drawnCard);
+
+                        if (isWildDrawFour)
+                        {
+                            int nextPlayerIndex = (game.GetPlayers().IndexOf(player) + game.GetTurnDirection() + game.GetPlayers().Count) % game.GetPlayers().Count;
+                            IPlayer nextPlayer = game.GetPlayers()[nextPlayerIndex];
+
+                            display.DisplayGameState(game, nextPlayer, false);
+
+                            if (display.AskForWildDrawFourChallenge(player))
+                            {
+                                bool challengeSuccessful = game.ChallengeOnDrawFour(nextPlayer);
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                display.DisplayMessage("\nChallenge declined. Wild Draw Four effect will be applied normally.", ConsoleColor.Yellow);
+                                game.ForcedDraw(nextPlayer, 4);
+                                display.DisplayMessage($"{nextPlayer.Name} draws 4 cards and loses their turn!", ConsoleColor.Magenta);
+                                game.EndTurn();
+                                game.NextTurn();
+                                game.EndTurn();
+                                Thread.Sleep(1000);
+                            }
+                        }
                     }
-                }
-                else
-                {
-                    display.DisplayMessage("\nThis card cannot be played.", ConsoleColor.Red);
-                    Thread.Sleep(1000);
                 }
             }
             else
