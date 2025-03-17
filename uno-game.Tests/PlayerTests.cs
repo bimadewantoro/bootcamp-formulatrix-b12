8f6d8b7e378578d55c92021fb11a904f61279a34
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-17 08:32:41 +0700

    feat: player tests

---
+++ uno-game.Tests/PlayerTests.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using UnoGame.Models;
+
+namespace uno_game.Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            int expectedId = 1;
+            string expectedName = "TestPlayer";
+
+            var player = new Player(expectedId, expectedName);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(player.Id, Is.EqualTo(expectedId));
+                Assert.That(player.Name, Is.EqualTo(expectedName));
+            });
+        }
+
+        [Test]
+        public void ToString_ReturnsPlayerNameAndId()
+        {
+            var player = new Player(42, "Alice");
+
+            string result = player.ToString();
+            Console.WriteLine(result);
+
+            Assert.That(result, Is.EqualTo("Player 42: Alice"));
+        }
+
+        [Test]
+        public void Equals_WithDifferentId_ReturnsFalse()
+        {
+            var player1 = new Player(1, "Player One");
+            var player2 = new Player(2, "Player One");
+
+            Assert.That(player1.Equals(player2), Is.False);
+        }
+    }
+}
