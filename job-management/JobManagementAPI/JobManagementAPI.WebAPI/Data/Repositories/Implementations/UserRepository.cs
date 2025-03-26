0fee0bdd82d033a51a503a537e387f5ab6bf3349
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 14:26:42 +0700

    feat: create user repository

---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Data/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using JobManagementAPI.WebAPI.Data.Repositories.Interfaces;
+using JobManagementAPI.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobManagementAPI.WebAPI.Data.Repositories.Implementations
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _dbSet
+                .Include(u => u.RefreshTokens)
+                .SingleOrDefaultAsync(u => u.Username.ToLower() == username.ToLower());
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _dbSet.SingleOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+        }
+
+        public async Task<bool> IsUsernameUniqueAsync(string username)
+        {
+            return !await _dbSet.AnyAsync(u => u.Username.ToLower() == username.ToLower());
+        }
+
+        public async Task<bool> IsEmailUniqueAsync(string email)
+        {
+            return !await _dbSet.AnyAsync(u => u.Email.ToLower() == email.ToLower());
+        }
+    }
+}
