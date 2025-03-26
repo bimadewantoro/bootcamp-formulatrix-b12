961471a218d9d1c2206207b4a94ea3caeadaefb5
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 14:27:13 +0700

    feat: refresh token repository

---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Data/Repositories/Implementations/RefreshTokenRepository.cs
@@ -0,0 +1,32 @@
+﻿using JobManagementAPI.WebAPI.Data.Repositories.Interfaces;
+using JobManagementAPI.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobManagementAPI.WebAPI.Data.Repositories.Implementations
+{
+    public class RefreshTokenRepository : Repository<RefreshToken>, IRefreshTokenRepository
+    {
+        public RefreshTokenRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<RefreshToken> GetByTokenAsync(string token)
+        {
+            return await _dbSet
+                .Include(rt => rt.User)
+                .SingleOrDefaultAsync(rt => rt.Token == token);
+        }
+
+        public async Task RevokeAllUserTokensAsync(Guid userId)
+        {
+            var userTokens = await _dbSet
+                .Where(rt => rt.UserId == userId && !rt.IsRevoked)
+                .ToListAsync();
+
+            foreach (var token in userTokens)
+            {
+                token.IsRevoked = true;
+            }
+        }
+    }
+}
---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Data/Repositories/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using JobManagementAPI.WebAPI.Models;
+
+namespace JobManagementAPI.WebAPI.Data.Repositories.Interfaces
+{
+    public interface IRefreshTokenRepository : IRepository<RefreshToken>
+    {
+        Task<RefreshToken> GetByTokenAsync(string token);
+        Task RevokeAllUserTokensAsync(Guid userId);
+    }
+}
