fc04c835603729f02661e8af0624adbbfee74181
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 20:06:09 +0700

    feat: add update job dto

---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/DTOs/Job/UpdateJobDto.cs
@@ -0,0 +1,21 @@
+namespace JobManagementAPI.WebAPI.Models.DTOs.Job
+{
+    public class UpdateJobDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Department { get; set; }
+
+        public string Location { get; set; }
+
+        public string JobType { get; set; }
+
+        public decimal? MinSalary { get; set; }
+
+        public decimal? MaxSalary { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
