918bed050bb87c1a8e408fbaeb09f8e777bffebd
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 14:20:48 +0700

    feat: create refresh token

---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/RefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace JobManagementAPI.WebAPI.Models
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
