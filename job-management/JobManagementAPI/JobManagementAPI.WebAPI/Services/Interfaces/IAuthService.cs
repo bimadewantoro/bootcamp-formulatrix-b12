d3d3dc33f92f0e6eec3f451ddafb8ef9f3286891
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 14:28:28 +0700

    feat: create auth service

---
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using JobManagementAPI.WebAPI.Models.DTOs.Auth;
+
+namespace JobManagementAPI.WebAPI.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterUserDto registerDto);
+        Task<AuthResponseDto> LoginAsync(LoginUserDto loginDto);
+        Task<bool> LogoutAsync(string refreshToken);
+        Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+    }
+}
