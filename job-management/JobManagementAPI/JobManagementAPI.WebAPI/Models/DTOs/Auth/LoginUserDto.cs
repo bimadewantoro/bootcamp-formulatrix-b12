67cbd70424486c0141b1ec6be1ddb8c59b3843be
Bima Dewantoro <bimadewantoro22@gmail.com>
2025-03-26 16:54:53 +0700

    feat: remove atribute at auth DTO

--- job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/DTOs/Auth/LoginUserDto.cs
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/DTOs/Auth/LoginUserDto.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace JobManagementAPI.WebAPI.Models.DTOs.Auth
+﻿namespace JobManagementAPI.WebAPI.Models.DTOs.Auth
 {
     public class LoginUserDto
     {
-        [Required]
         public string Username { get; set; }
 
-        [Required]
         public string Password { get; set; }
     }
 }
--- job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/DTOs/Auth/RegisterUserDto.cs
+++ job-management/JobManagementAPI/JobManagementAPI.WebAPI/Models/DTOs/Auth/RegisterUserDto.cs
@@ -4,24 +4,14 @@ namespace JobManagementAPI.WebAPI.Models.DTOs.Auth
 {
     public class RegisterUserDto
     {
-        [Required]
-        [StringLength(50)]
         public string Username { get; set; }
 
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 6)]
         public string Password { get; set; }
 
-        [Required]
-        [StringLength(50)]
         public string FirstName { get; set; }
 
-        [Required]
-        [StringLength(50)]
         public string LastName { get; set; }
     }
 }
